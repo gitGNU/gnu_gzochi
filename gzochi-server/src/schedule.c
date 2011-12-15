7e9fb4764b0fbe74f35705d113e75786a6861097
Julian Graham <julian@navigator.(none)>
2011-12-14 21:51:36 -0500

    Initial commit for refactoring task execution: Add first-class scheduling system with primitives for queueing and consuming tasks.
    * gzochi-server/src/schedule.c, schedule.h: New files.
    * gzochi-server/src/Makefile.am: Add schedule.c to gzochid_SOURCES.

---
+++ gzochi-server/src/schedule.c
@@ -0,0 +1,160 @@
+/* schedule.c: Task execution and task queue management routines for gzochid
+ * Copyright (C) 2011 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+
+#include "guile.h"
+#include "schedule.h"
+#include "task.h"
+
+static void pending_task_executor (gpointer data, gpointer user_data)
+{
+  gzochid_pending_task *pending_task = (gzochid_pending_task *) data;
+
+  g_mutex_lock (pending_task->mutex);
+  pending_task->task->worker (pending_task->task->data, user_data);
+  pending_task->state = GZOCHID_PENDING_TASK_STATE_COMPLETED;
+  g_cond_broadcast (pending_task->cond);
+  g_mutex_unlock (pending_task->mutex);
+}
+
+gpointer gzochid_schedule_task_executor (gpointer data)
+{
+  gzochid_task_queue *task_queue = (gzochid_task_queue *) data;
+
+  g_mutex_lock (task_queue->mutex);
+  while (TRUE)
+    {
+      if (g_queue_is_empty (task_queue->queue))
+	g_cond_wait (task_queue->cond, task_queue->mutex);
+      else 
+	{
+	  struct timeval current_time;
+	  GTimeVal wait_time;
+	  gzochid_pending_task *pending_task = 
+	    (gzochid_pending_task *) g_queue_peek_head (task_queue->queue);
+
+	  gettimeofday (&current_time, NULL);
+
+	  if (current_time.tv_sec 
+	      > pending_task->scheduled_execution_time.tv_sec
+	      || (current_time.tv_sec 
+		  == pending_task->scheduled_execution_time.tv_sec
+		  && (current_time.tv_usec 
+		      > pending_task->scheduled_execution_time.tv_usec)))
+	    {
+	      pending_task = (gzochid_pending_task *) 
+		g_queue_pop_head (task_queue->queue);
+	      gzochid_guile_thread_pool_push 
+		(task_queue->pool, pending_task_executor, pending_task, NULL);
+	    }
+	  else 
+	    {
+	      wait_time.tv_sec = 
+		pending_task->scheduled_execution_time.tv_sec 
+		- current_time.tv_sec;
+	      wait_time.tv_usec = 
+		pending_task->scheduled_execution_time.tv_usec 
+		- current_time.tv_usec;
+
+	      g_cond_timed_wait 
+		(task_queue->cond, task_queue->mutex, &wait_time);
+	    }
+	}
+    }
+  g_mutex_unlock (task_queue->mutex);
+
+  return NULL;
+}
+
+void gzochid_schedule_task_queue_start (gzochid_task_queue *task_queue)
+{
+  if (task_queue->consumer_thread == NULL)
+    task_queue->consumer_thread = g_thread_create 
+      (gzochid_schedule_task_executor, task_queue, FALSE, NULL);
+}
+
+gzochid_pending_task *gzochid_schedule_submit_task
+(gzochid_task_queue *task_queue, gzochid_task *task)
+{
+  return gzochid_schedule_submit_delayed_task (task_queue, task, 0);
+}
+
+gzochid_pending_task *gzochid_pending_task_new 
+(gzochid_task *task, struct timeval scheduled_execution_time)
+{
+  gzochid_pending_task *pending_task = malloc (sizeof (gzochid_pending_task));
+
+  pending_task->task = task;
+  pending_task->state = GZOCHID_PENDING_TASK_STATE_PENDING;
+  pending_task->cond = g_cond_new ();
+  pending_task->mutex = g_mutex_new ();
+
+  pending_task->scheduled_execution_time = scheduled_execution_time;
+  
+  return pending_task;
+}
+
+gint pending_task_compare (gconstpointer a, gconstpointer b, gpointer user_data)
+{
+  gzochid_pending_task *pending_task_a = (gzochid_pending_task *) a;
+  gzochid_pending_task *pending_task_b = (gzochid_pending_task *) b;
+
+  if (pending_task_a->scheduled_execution_time.tv_sec <
+      pending_task_b->scheduled_execution_time.tv_sec)
+    return -1;
+  else if (pending_task_a->scheduled_execution_time.tv_sec 
+	   > pending_task_b->scheduled_execution_time.tv_sec)
+    return 1;
+  else return pending_task_a->scheduled_execution_time.tv_usec 
+	 - pending_task_b->scheduled_execution_time.tv_usec;
+}
+
+gzochid_pending_task *gzochid_schedule_submit_delayed_task
+(gzochid_task_queue *task_queue, gzochid_task *task, long delay)
+{
+  struct timeval target_time;
+  gzochid_pending_task *pending_task = NULL;
+
+  gettimeofday (&target_time, NULL);
+
+  target_time.tv_sec += delay / 1000;
+  target_time.tv_usec += (delay % 1000) * 1000;
+
+  pending_task = gzochid_pending_task_new (task, target_time);
+  
+  g_mutex_lock (task_queue->mutex);
+  g_queue_insert_sorted 
+    (task_queue->queue, pending_task, pending_task_compare, NULL);
+  g_cond_signal (task_queue->cond);
+  g_mutex_unlock (task_queue->mutex);
+
+  return pending_task;
+}
+
+void gzochid_schedule_run_task 
+(gzochid_task_queue *task_queue, gzochid_task *task)
+{
+  gzochid_pending_task *pending_task = 
+    gzochid_schedule_submit_task (task_queue, task);
+
+  g_mutex_lock (pending_task->mutex);
+  while (pending_task->state == GZOCHID_PENDING_TASK_STATE_PENDING)
+    g_cond_wait (pending_task->cond, pending_task->mutex);
+  g_mutex_unlock (pending_task->mutex);
+  free (pending_task);
+}
---
+++ gzochi-server/src/schedule.h
@@ -0,0 +1,64 @@
+/* schedule.h: Prototypes and declarations for schedule.c
+ * Copyright (C) 2011 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GZOCHID_SCHEDULE_H
+#define GZOCHID_SCHEDULE_H
+
+#include <glib.h>
+
+#include "task.h"
+
+enum gzochid_pending_task_state
+  {
+    GZOCHID_PENDING_TASK_STATE_PENDING,
+    GZOCHID_PENDING_TASK_STATE_COMPLETED,
+    GZOCHID_PENDING_TASK_STATE_ERROR
+  };
+
+typedef struct _gzochid_task_queue
+{
+  GCond *cond;
+  GMutex *mutex;
+  GThread *consumer_thread;
+
+  GQueue *queue;
+  GThreadPool *pool;
+} gzochid_task_queue;
+
+typedef struct _gzochid_pending_task
+{
+  GCond *cond;
+  GMutex *mutex;
+  
+  struct timeval scheduled_execution_time;
+  enum gzochid_pending_task_state state;
+
+  gzochid_task *task;
+} gzochid_pending_task;
+
+gpointer gzochid_schedule_task_executor (gpointer);
+
+gzochid_task_queue *gzochid_schedule_task_queue_new (GThreadPool *);
+void gzochid_schedule_task_queue_start (gzochid_task_queue *);
+
+gzochid_pending_task *gzochid_schedule_submit_task 
+(gzochid_task_queue *, gzochid_task *);
+gzochid_pending_task *gzochid_schedule_submit_delayed_task 
+(gzochid_task_queue *, gzochid_task *, long);
+void gzochid_schedule_run_task (gzochid_task_queue *, gzochid_task *);
+
+#endif /* GZOCHID_SCHEDULE_H */
