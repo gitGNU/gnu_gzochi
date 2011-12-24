2f4709b442d759417a8dd0e2a15521a0a2719c73
Julian Graham <julian@navigator.(none)>
2011-12-24 00:53:00 -0500

    Add some rudimentary store-browsing capabilities to the built-in HTTP server context; just list binding names for the moment.
    * gzochi-server/src/httpd.c (dispatch_names, dispatch_app): New functions.
      (dispatch): Add hook for URLs that begin with "/app/".
      (initialize): Pass the HTTPD context pointer itself as user data to the
      MHD daemon.

--- gzochi-server/src/httpd.c
+++ gzochi-server/src/httpd.c
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <glib.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -26,11 +28,74 @@
 #include <microhttpd.h>
 
 #include "admin.h"
+#include "app.h"
 #include "context.h"
 #include "fsm.h"
+#include "game.h"
+#include "gzochid.h"
 #include "log.h"
 #include "httpd.h"
+#include "storage.h"
 #include "threads.h"
+
+static int dispatch_names (struct MHD_Connection *connection, 
+			   gzochid_application_context *context)
+{
+  GString *response_str = g_string_new (NULL);
+  struct MHD_Response *response = NULL;
+  
+  int klen = 0;
+  char *k = gzochid_storage_first_key (context->names, &klen);
+
+  g_string_append (response_str, "<html>\n");
+  g_string_append (response_str, "  <body>\n");
+  g_string_append (response_str, "    <ul>\n");
+
+  while (k != NULL)
+    {
+      char *next_k = NULL;
+
+      g_string_append (response_str, "      <li>");
+      g_string_append_len (response_str, k, klen);
+      g_string_append (response_str, "</li>\n");
+      
+      next_k = gzochid_storage_next_key (context->names, k, klen, &klen);
+
+      free (k);
+      k = next_k;
+    }
+
+  g_string_append (response_str, "    </ul>\n");
+  g_string_append (response_str, "  </body>\n");
+  g_string_append (response_str, "</html>");
+
+  response = MHD_create_response_from_data 
+    (response_str->len, response_str->str, TRUE, TRUE);
+
+  return MHD_queue_response (connection, MHD_HTTP_OK, response);
+}
+
+static int dispatch_app (struct MHD_Connection *connection, const char *url,
+			 gzochid_game_context *game_context)
+{
+  gzochid_application_context *app_context = NULL;
+  const char *app_url = url + 5;
+  int app_len = 0;
+
+  char *rest = strchr (app_url, '/');
+  char *name = NULL;
+  
+  app_len = rest - app_url;
+  name = calloc (app_len + 1, sizeof (char));  
+  name = strncpy (name, app_url, app_len);
+
+  app_context = gzochid_game_context_lookup_application (game_context, name);
+  free (name);
+  
+  if (app_context != NULL)
+    return dispatch_names (connection, app_context);
+  else return 404;
+}
 
 static int dispatch (void *cls, struct MHD_Connection *connection, 
 		     const char *url, const char *method, const char *version, 
@@ -39,6 +104,7 @@ static int dispatch (void *cls, struct MHD_Connection *connection,
 {
   int ret = MHD_NO;
   struct MHD_Response *response = NULL;
+  gzochid_httpd_context *context = (gzochid_httpd_context *) cls;
 
   if (strcmp (method, "GET") != 0)
     return ret;
@@ -49,6 +115,15 @@ static int dispatch (void *cls, struct MHD_Connection *connection,
       response = MHD_create_response_from_data 
 	(strlen (page), (void *) page, TRUE, TRUE);
       ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+    }
+  else if (strncmp (url, "/app/", 5) == 0)
+    {
+      gzochid_admin_context *admin_context = 
+	(gzochid_admin_context *) ((gzochid_context *) context)->parent;
+      gzochid_server_context *server_context =
+	(gzochid_server_context *) ((gzochid_context *) admin_context)->parent; 
+
+      return dispatch_app (connection, url, server_context->game_context);
     }
   else 
     {
@@ -77,8 +152,8 @@ static void initialize (int from_state, int to_state, gpointer user_data)
     (gzochid_admin_context *) context->parent;
 
   httpd_context->daemon = MHD_start_daemon 
-    (MHD_USE_SELECT_INTERNALLY, httpd_context->port, NULL, NULL, &dispatch, 
-     NULL, MHD_OPTION_END);
+    (MHD_USE_SELECT_INTERNALLY, httpd_context->port, NULL, NULL, dispatch, 
+     httpd_context, MHD_OPTION_END);
 
   gzochid_notice ("HTTP server listening on port %d", httpd_context->port);
 
