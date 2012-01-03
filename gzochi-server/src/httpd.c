7f51e73851f1e4cef3066cd3ebadc1d4c9db112d
Julian Graham <julian@navigator.(none)>
2012-01-02 23:25:58 -0500

    Add some more powerful data browsing functionality to gzochid's management web server.
    * gzochi-server/src/httpd.c (data_state): New data structure.
      (next_line, write_data_line, free_data_state, dispatch_oid,
      dispatch_oids): New functions.
      (dispatch_app): Add dispatch to oid handlers.

--- gzochi-server/src/httpd.c
+++ gzochi-server/src/httpd.c
@@ -16,9 +16,11 @@
  */
 
 #include <glib.h>
+#include <gmp.h>
 
-#include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <stdint.h>
@@ -37,6 +39,224 @@
 #include "httpd.h"
 #include "storage.h"
 #include "threads.h"
+
+#define OID_PREFIX_LEN 26
+#define OID_SUFFIX_LEN 29
+#define OID_LINE_LEN 80
+
+struct data_state 
+{
+  char *data;
+  int data_offset;
+  int data_length;
+
+  char *current_line;
+  int current_line_offset;
+  int current_line_length;
+};
+
+static void next_line (struct data_state *state)
+{
+  char *buf = NULL;
+  int i = 0, remaining = state->data_length - state->data_offset,
+    num_bytes = MIN (16, remaining);
+  gboolean needs_prefix = state->data_offset == 0;
+  gboolean needs_suffix = remaining <= 16;
+
+  int line_length = OID_LINE_LEN - (16 - num_bytes);
+  
+  if (state->data_offset == state->data_length)
+    return;
+
+  if (needs_prefix)
+    line_length += OID_PREFIX_LEN;
+  if (needs_suffix)
+    line_length += OID_SUFFIX_LEN;
+  
+  state->current_line = malloc (sizeof (char) * (line_length + 1));
+  state->current_line_length = line_length + 1;
+
+  buf = state->current_line;
+
+  if (needs_prefix)
+    {
+      snprintf (buf, 8, "<html>\n");
+      snprintf (buf + 7, 10, "  <body>\n");
+      snprintf (buf + 16, 11, "    <pre>\n");
+      buf += OID_PREFIX_LEN;
+    }
+
+  snprintf (buf, 13, "%.8x    ", state->data_offset);
+  buf += 12;
+  
+  for (i = 0; i < num_bytes; i++)
+    {
+      snprintf (buf, 4, "%.2x ", state->data[state->data_offset + i]);
+      buf += 3;
+
+      if ((i + 1) % 4 == 0)
+	{
+	  snprintf (buf, 2, " ");
+	  buf += 1;
+	}
+    }
+
+  for (i = num_bytes; i < 16; i++)
+    {
+      snprintf (buf, 4, "   ");
+      buf += 3;
+
+      if ((i + 1) % 4 == 0)
+	{
+	  snprintf (buf, 2, " ");
+	  buf += 1;
+	}
+    }
+
+  for (i = 0; i < num_bytes; i++)
+    {
+      char c = state->data[state->data_offset + i];
+      snprintf (buf++, 2, "%c", isgraph (c) ? c : '.');
+    }
+
+  snprintf (buf++, 2, "\n");
+  
+  state->data_offset += num_bytes;
+
+  if (needs_suffix)
+    {
+      snprintf (buf, 12, "    </pre>\n");
+      snprintf (buf + 11, 11, "  </body>\n");
+      snprintf (buf + 21, 9, "</html>\n");
+      buf += OID_SUFFIX_LEN;
+    }
+}
+
+static int write_data_line (void *cls, uint64_t pos, char *buf, int max)
+{
+  int n = 0;
+  struct data_state *state = (struct data_state *) cls;
+
+  if (state->current_line == NULL)
+    next_line (state);
+  if (state->current_line == NULL)
+    return -1;
+
+  n = MIN (max, state->current_line_length - state->current_line_offset);
+  memcpy (buf, state->current_line + state->current_line_offset, n);
+  state->current_line_offset += n;
+
+  if (state->current_line_offset == state->current_line_length)
+    {
+      free (state->current_line);
+      
+      state->current_line = NULL;
+      state->current_line_length = 0;
+      state->current_line_offset = 0;
+    }
+
+  return n;
+}
+
+static void free_data_state (void *ptr)
+{
+  struct data_state *state = (struct data_state *) ptr;
+
+  free (state->data);
+
+  if (state->current_line != NULL)
+    free (state->current_line);
+
+  free (state);
+}
+
+static int dispatch_oid (struct MHD_Connection *connection, 
+			 gzochid_application_context *context, mpz_t oid)
+{
+  int data_length = 0;
+  char *oid_str = mpz_get_str (NULL, 16, oid);
+  char *data = gzochid_storage_get 
+    (context->oids, oid_str, strlen (oid_str) + 1, &data_length);
+  struct data_state *state = calloc (1, sizeof (struct data_state));
+  struct MHD_Response *response = NULL;
+
+  state->data = data;
+  state->data_length = data_length;
+
+  response = MHD_create_response_from_callback 
+    (-1, OID_PREFIX_LEN + OID_LINE_LEN + OID_SUFFIX_LEN, write_data_line, 
+     state, free_data_state);
+
+  return MHD_queue_response (connection, MHD_HTTP_OK, response);
+}
+
+static int dispatch_oids 
+(struct MHD_Connection *connection, const char *url, const char *path,
+ gzochid_application_context *context)
+{
+  if (path == NULL)
+    {
+      GString *url_str = g_string_new (url);
+      struct MHD_Response *response = MHD_create_response_from_data 
+	(0, "", FALSE, FALSE);
+      
+      g_string_append (url_str, "/");
+      MHD_add_response_header (response, "Location", url_str->str);
+      g_string_free (url_str, TRUE);
+      
+      return MHD_queue_response 
+	(connection, MHD_HTTP_MOVED_PERMANENTLY, response);
+    }
+  else if (strlen (path) > 1)
+    {
+      mpz_t oid;
+      int ret = 0;
+
+      mpz_init (oid);
+      mpz_set_str (oid, path + 1, 16);
+      ret = dispatch_oid (connection, context, oid);
+      mpz_clear (oid);
+
+      return ret;
+    }
+  else
+    {
+      GString *response_str = g_string_new (NULL);
+      struct MHD_Response *response = NULL;
+      
+      int klen = 0;
+      char *k = gzochid_storage_first_key (context->oids, &klen);
+      
+      g_string_append (response_str, "<html>\n");
+      g_string_append (response_str, "  <body>\n");
+      g_string_append (response_str, "    <ul>\n");
+      
+      while (k != NULL)
+	{
+	  char *next_k = NULL;
+	  
+	  g_string_append (response_str, "      <li><a href=\"");
+	  g_string_append_len (response_str, k, klen - 1);
+	  g_string_append (response_str, "\">");
+	  g_string_append_len (response_str, k, klen - 1);
+	  g_string_append (response_str, "</a></li>\n");
+	  
+	  next_k = gzochid_storage_next_key (context->oids, k, klen, &klen);
+	  
+	  free (k);
+	  k = next_k;
+	}
+
+      g_string_append (response_str, "    </ul>\n");
+      g_string_append (response_str, "  </body>\n");
+      g_string_append (response_str, "</html>");
+      
+      response = MHD_create_response_from_data 
+	(response_str->len, response_str->str, TRUE, TRUE);
+      
+      return MHD_queue_response (connection, MHD_HTTP_OK, response);
+    }
+}
 
 static int dispatch_names (struct MHD_Connection *connection, 
 			   gzochid_application_context *context)
@@ -93,7 +313,15 @@ static int dispatch_app (struct MHD_Connection *connection, const char *url,
   free (name);
   
   if (app_context != NULL)
-    return dispatch_names (connection, app_context);
+    {
+      char *operation = strchr (rest, '/');
+      if (strncmp (operation, "/names", 6) == 0)
+	return dispatch_names (connection, app_context);
+      else if (strncmp (operation, "/oids", 5) == 0)
+	return dispatch_oids 
+	  (connection, url, strchr (operation + 1, '/'), app_context);
+      else return 404;
+    }
   else return 404;
 }
 
