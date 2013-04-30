19f2596a63992c056df4a52884477368d0e05355
Julian Graham <julian@navigator.(none)>
2013-04-29 20:16:03 -0400

    Initial attempt to create an application event bus to drive stats reporting in the web console.
    * gzochi-server/src/Makefile.am (noinst_HEADERS): Add `event.h'.
      (gzochid_SOURCES): Add `event.c'.
      Add dependency for `event.c' on `event.h'.
    * gzochi-server/src/event.c, event.h: New files.
    * gzochi-server/src/Makefile.am (test_programs): Add `test-event'.
      (test_event_SOURCES, test_event_LDADD): New target declarations.
    * gzochi-server/src/tests/test-event.c: New file.

---
+++ gzochi-server/src/event.c
@@ -0,0 +1,141 @@
+/* event.c: Application event bus implementation for gzochid
+ * Copyright (C) 2013 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "app.h"
+#include "event.h"
+
+typedef struct _gzochid_application_event_handler_registration
+{
+  gzochid_application_event_handler handler;
+  gpointer user_data;
+} gzochid_application_event_handler_registration;
+
+struct _gzochid_application_event_source
+{
+  GSource base;
+  gzochid_application_context *context;
+  GList *handlers;
+  GQueue *events;
+  GMutex *mutex;
+};
+
+static gboolean prepare (GSource *source, gint *timeout)
+{
+  gzochid_application_event_source *event_source = 
+    (gzochid_application_event_source *) source;
+  
+  return !g_queue_is_empty (event_source->events);
+}
+
+static gboolean check (GSource *source)
+{
+  gzochid_application_event_source *event_source = 
+    (gzochid_application_event_source *) source;
+  
+  return !g_queue_is_empty (event_source->events);
+}
+
+static void dispatch_inner_inner (gpointer data, gpointer user_data)
+{
+  gzochid_application_event_handler_registration *registration = 
+    (gzochid_application_event_handler_registration *) data;
+
+  gpointer *context_and_event = (gpointer *) user_data;
+  gzochid_application_context *context = 
+    (gzochid_application_context *) context_and_event[0];
+  gzochid_application_event *event = 
+    (gzochid_application_event *) context_and_event[1];
+      
+  registration->handler (context, event, registration->user_data);
+}
+
+static void dispatch_inner (gpointer data, gpointer user_data)
+{
+  gzochid_application_event_source *event_source = 
+    (gzochid_application_event_source *) user_data;
+  
+  gpointer context_and_event[2];
+  context_and_event[0] = event_source->context;
+  context_and_event[1] = data;
+
+  g_list_foreach 
+    (event_source->handlers, dispatch_inner_inner, context_and_event);
+
+  free (data);
+}
+
+static gboolean dispatch (GSource *source, GSourceFunc cb, gpointer user_data)
+{
+  gzochid_application_event_source *event_source = 
+    (gzochid_application_event_source *) source;
+
+  g_mutex_lock (event_source->mutex);
+  g_queue_foreach (event_source->events, dispatch_inner, event_source);
+  g_queue_clear (event_source->events);
+  g_mutex_unlock (event_source->mutex);
+
+  return TRUE;
+}
+
+static void finalize (GSource *source)
+{
+}
+
+static GSourceFuncs event_funcs = { prepare, check, dispatch, finalize };
+
+gzochid_application_event_source *gzochid_application_event_source_new 
+(gzochid_application_context *context)
+{
+  gzochid_application_event_source *source = 
+    (gzochid_application_event_source *) g_source_new
+    (&event_funcs, sizeof (gzochid_application_event_source));
+
+  source->context = context;
+  source->events = g_queue_new ();
+  source->handlers = NULL;
+  source->mutex = g_mutex_new ();
+  
+  return source;
+}
+
+void gzochid_application_event_attach 
+(gzochid_application_event_source *source, 
+ gzochid_application_event_handler handler, gpointer user_data)
+{
+  gzochid_application_event_handler_registration *registration = 
+    malloc (sizeof (gzochid_application_event_handler_registration));
+
+  registration->handler = handler;
+  registration->user_data = user_data;
+
+  source->handlers = g_list_append (source->handlers, registration);
+}
+
+void gzochid_application_event_dispatch
+(gzochid_application_event_source *source, gzochid_application_event *event)
+{
+  gzochid_application_event_source *event_source = 
+    (gzochid_application_event_source *) source;
+
+  g_mutex_lock (event_source->mutex);
+  g_queue_push_tail (event_source->events, event);
+  g_mutex_unlock (event_source->mutex);
+}
---
+++ gzochi-server/src/event.h
@@ -0,0 +1,64 @@
+/* event.h: Prototypes and declarations for event.c
+ * Copyright (C) 2013 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GZOCHID_EVENT_H
+#define GZOCHID_EVENT_H
+
+#include <glib.h>
+#include <sys/time.h>
+
+#include "app.h"
+
+typedef enum _gzochid_application_event_type
+  {
+    TRANSACTION_START, /* An application transaction has been started. */
+    TRANSACTION_COMMIT, /* A transaction has been committed. */
+    TRANSACTION_ROLLBACK /* A transaction has been rolled back. */
+  }
+  gzochid_application_event_type;
+
+typedef struct _gzochid_application_event
+{
+  gzochid_application_event_type type;
+  struct timeval timestamp;
+} gzochid_application_event;
+
+typedef struct _gzochid_application_event_source 
+gzochid_application_event_source;
+
+gzochid_application_event_source *gzochid_application_event_source_new 
+(gzochid_application_context *);
+
+typedef void (*gzochid_application_event_handler) 
+(gzochid_application_context *, gzochid_application_event *, gpointer);
+
+/**
+   Attaches the specified application event handler to the event source.
+ */
+void gzochid_application_event_attach 
+(gzochid_application_event_source *, gzochid_application_event_handler, 
+ gpointer);
+
+/**
+   Dispatches an event to the event source, triggering all registered handlers.
+   The application event's memory will be freed once all handlers have been
+   invoked.
+ */
+void gzochid_application_event_dispatch
+(gzochid_application_event_source *, gzochid_application_event *);
+
+#endif /* GZOCHID_EVENT_H */
