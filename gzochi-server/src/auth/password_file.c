f02133743cd46cbdf768f714e1b43b208ef0d7ca
Julian Graham <julian@navigator.(none)>
2013-05-12 17:28:29 -0400

    New `password file' authentication plugin.
    * gzochi-server/configure.ac (AC_CONFIG_FILES): Product Makefiles in
      `src/auth' and `src/tests/auth'.
    * gzochi-server/src/Makefile.am (SUBDIRS): Add `auth' subdirectory.
    * gzochi-server/src/auth/Makefile.am, password_file.c: New files.
    * gzochi-server/src/tests/Makefile.am (SUBDIRS): Add `auth' subdirectory.
    * gzochi-server/src/tests/auth/Makefile.am, passwords.txt,
      test-password_file.c: New files.

---
+++ gzochi-server/src/auth/password_file.c
@@ -0,0 +1,113 @@
+/* password_file.c: Password file-based authentication plugin for gzochid
+ * Copyright (C) 2013 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../auth.h"
+
+#define PATH_PROPERTY "path"
+
+static gpointer initialize (GHashTable *properties, GError **error)
+{
+  gchar *path = NULL;
+  gchar *line = NULL;
+  GIOChannel *channel = NULL;
+  GError *channel_error = NULL;
+  GHashTable *passwords = g_hash_table_new (g_str_hash, g_str_equal);
+
+  if (! g_hash_table_contains (properties, PATH_PROPERTY))
+    {
+      g_set_error_literal 
+	(error, 
+	 GZOCHID_AUTH_PLUGIN_ERROR, 
+	 GZOCHID_AUTH_PLUGIN_ERROR_INIT,
+	 "Required 'path' property not set.");
+		   
+      return NULL;
+    }
+  
+  path = (gchar *) g_hash_table_lookup (properties, PATH_PROPERTY);
+  channel = g_io_channel_new_file (path, "r", &channel_error);
+  
+  if (channel == NULL)
+    {
+      g_propagate_error (error, channel_error);
+      return NULL;
+    }
+
+  while (g_io_channel_read_line (channel, &line, NULL, NULL, NULL) 
+	 == G_IO_STATUS_NORMAL)
+    {
+      gchar **tokens = g_strsplit (line, "=", 2);
+      if (tokens[0] != NULL && tokens[1] != NULL)
+	{
+	  g_hash_table_insert (passwords, tokens[0], g_strchomp (tokens[1]));
+	  g_free (tokens);
+	}
+      else g_strfreev (tokens);
+      g_free (line);
+    }
+  
+  g_io_channel_shutdown (channel, FALSE, NULL);
+  g_io_channel_unref (channel);
+
+  return passwords;
+}
+
+static gzochid_auth_identity *authenticate 
+(unsigned char *credentials, short len, gpointer auth_data, GError **error)
+{
+  GHashTable *passwords = (GHashTable *) auth_data;
+  unsigned char *pivot = (unsigned char *) memchr (credentials, '\0', len);
+
+  if (pivot == NULL)
+    {
+      g_set_error_literal 
+	(error,
+	 GZOCHID_AUTH_PLUGIN_ERROR, 
+	 GZOCHID_AUTH_PLUGIN_ERROR_AUTH,
+	 "Improperly formatted credentials.");
+
+      return NULL;
+    }
+
+  if (g_hash_table_contains (passwords, credentials))
+    {
+      char *password = g_hash_table_lookup (passwords, credentials);
+      int cmplen = MIN (len - (pivot - credentials), strlen (password));
+
+      if (strncmp (password, (char *) pivot + 1, cmplen) == 0)
+	{
+	  gzochid_auth_identity *identity = 
+	    malloc (sizeof (gzochid_auth_identity));
+
+	  identity->name = strdup ((char *) credentials);
+	  return identity;
+	}
+      else return NULL;
+    }
+  else return NULL;
+}
+
+static gzochid_auth_plugin_info info = 
+  { "password_file", initialize, authenticate };
+
+GZOCHID_AUTH_INIT_PLUGIN (info)
---
+++ gzochi-server/src/tests/auth/test-password_file.c
@@ -0,0 +1,156 @@
+/* test-data.c: Test routines for data.c in gzochid.
+ * Copyright (C) 2013 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "../../auth.h"
+
+gint gzochid_auth_init_plugin (gzochid_auth_plugin *);
+
+static void test_initialize ()
+{
+  GError *error = NULL;
+  GHashTable *properties = g_hash_table_new (g_str_hash, g_str_equal);
+  GHashTable *passwords = NULL;
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+
+  g_hash_table_insert (properties, "path", "passwords.txt");
+
+  gzochid_auth_init_plugin (plugin);
+  passwords = (GHashTable *) plugin->info->initialize (properties, &error);
+
+  g_assert (passwords != NULL);
+  g_assert_no_error (error);
+  g_assert_cmpint (g_hash_table_size (passwords), ==, 3);
+
+  g_assert_cmpstr (g_hash_table_lookup (passwords, "foo"), ==, "bar");
+  g_assert_cmpstr (g_hash_table_lookup (passwords, "bar"), ==, "baz");
+  g_assert_cmpstr (g_hash_table_lookup (passwords, "baz"), ==, "qux");
+
+  free (plugin);
+  g_hash_table_destroy (properties);
+  g_hash_table_destroy (passwords);
+}
+
+static void test_initialize_missing_path ()
+{
+  GError *error = NULL;
+  GHashTable *properties = g_hash_table_new (g_str_hash, g_str_equal);
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+
+  gzochid_auth_init_plugin (plugin);
+  g_assert (plugin->info->initialize (properties, &error) == NULL);
+  g_assert_error 
+    (error, GZOCHID_AUTH_PLUGIN_ERROR, GZOCHID_AUTH_PLUGIN_ERROR_INIT);
+
+  g_clear_error (&error);
+  free (plugin);
+  g_hash_table_destroy (properties);
+}
+
+static void test_initialize_missing_file ()
+{
+  GError *error = NULL;
+  GHashTable *properties = g_hash_table_new (g_str_hash, g_str_equal);
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+
+  g_hash_table_insert (properties, "path", "passwords2.txt");
+
+  gzochid_auth_init_plugin (plugin);
+  g_assert (plugin->info->initialize (properties, &error) == NULL);
+  g_assert_error (error, G_FILE_ERROR, G_FILE_ERROR_NOENT);
+
+  g_clear_error (&error);
+  free (plugin);
+  g_hash_table_destroy (properties);
+}
+
+static void test_authenticate ()
+{
+  GError *error = NULL;
+  GHashTable *passwords = g_hash_table_new (g_str_hash, g_str_equal);
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+  gzochid_auth_identity *identity = NULL;
+
+  g_hash_table_insert (passwords, "foo", "bar");
+
+  gzochid_auth_init_plugin (plugin);
+  identity = plugin->info->authenticate ("foo\0bar", 7, passwords, &error);
+  g_assert (identity != NULL);
+  g_assert_no_error (error);
+  
+  g_assert_cmpstr (identity->name, ==, "foo");
+
+  free (identity);
+  free (plugin);
+  g_hash_table_destroy (passwords);
+}
+
+static void test_authenticate_fail ()
+{
+  GError *error = NULL;
+  GHashTable *passwords = g_hash_table_new (g_str_hash, g_str_equal);
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+
+  gzochid_auth_init_plugin (plugin);
+  g_assert 
+    (plugin->info->authenticate ("foo\0bar", 7, passwords, &error) == NULL);
+  g_assert_no_error (error);
+
+  free (plugin);
+  g_hash_table_destroy (passwords);
+}
+
+static void test_authenticate_error ()
+{
+  GError *error = NULL;
+  GHashTable *passwords = g_hash_table_new (g_str_hash, g_str_equal);
+  gzochid_auth_plugin *plugin = malloc (sizeof (gzochid_auth_plugin));
+
+  gzochid_auth_init_plugin (plugin);
+  g_assert 
+    (plugin->info->authenticate ("foobar", 6, passwords, &error) == NULL);
+  g_assert_error 
+    (error, GZOCHID_AUTH_PLUGIN_ERROR, GZOCHID_AUTH_PLUGIN_ERROR_AUTH);
+  g_clear_error (&error);
+
+  free (plugin);
+  g_hash_table_destroy (passwords);
+}
+
+int main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/auth/password_file/initialize", test_initialize);
+  g_test_add_func 
+    ("/auth/password_file/initialize/missing_path", 
+     test_initialize_missing_path);
+  g_test_add_func 
+    ("/auth/password_file/initialize/missing_file", 
+     test_initialize_missing_file);
+
+  g_test_add_func ("/auth/password_file/authenticate", test_authenticate);
+  g_test_add_func 
+    ("/auth/password_file/authenticate/fail", test_authenticate_fail);
+  g_test_add_func
+    ("/auth/password_file/authenticate/error", test_authenticate_error);
+
+  return g_test_run ();
+}
