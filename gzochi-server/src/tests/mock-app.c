22a0432b68b1aa48e987572487680844aea8a0f9
Julian Graham <julian@navigator.example.com>
2013-06-25 00:32:23 -0400

    Add some additional functionality to the mock application module.
    * gzochi-server/src/tests/mock-app.c (execution_new,
      transactional_task_worker,
      gzochid_application_transactional_task_worker): New functions.
      (gzochid_transactional_application_task_execution_new,
      gzochid_transactional_application_task_timed_execution_new): Return a new
      execution.
      (gzochid_application_resubmitting_transactional_task_worker): Provide an
      actual implementation.

--- gzochi-server/src/tests/mock-app.c
+++ gzochi-server/src/tests/mock-app.c
@@ -29,6 +29,10 @@ gzochid_application_context *gzochid_application_context_new ()
   gzochid_application_context *context = 
     calloc (1, sizeof (gzochid_application_context));
 
+  context->free_oids_lock = g_mutex_new ();
+  context->client_mapping_lock = g_mutex_new ();
+
+  context->meta = gzochid_storage_open ("/dev/null");
   context->oids = gzochid_storage_open ("/dev/null");
   context->names = gzochid_storage_open ("/dev/null");
 
@@ -46,18 +50,40 @@ void gzochid_application_task_thread_worker (gpointer data, gpointer user_data)
   gzochid_application_task_worker (data);
 }
 
+static gzochid_transactional_application_task_execution *execution_new
+(gzochid_application_task *task, struct timeval *timeout)
+{
+  gzochid_transactional_application_task_execution *execution = 
+    malloc (sizeof (gzochid_transactional_application_task_execution));
+
+  execution->task = task;
+
+  if (timeout != NULL)
+    {
+      execution->timeout = malloc (sizeof (struct timeval));
+      execution->timeout->tv_sec = timeout->tv_sec;
+      execution->timeout->tv_usec = timeout->tv_usec;
+    }
+  else execution->timeout = NULL;
+
+  execution->attempts = 0;
+  execution->result = GZOCHID_TRANSACTION_PENDING;
+
+  return execution;
+}
+
 gzochid_transactional_application_task_execution *
 gzochid_transactional_application_task_execution_new 
 (gzochid_application_task *task)
 {
-  return NULL;
+  return execution_new (task, NULL);
 }
 
 gzochid_transactional_application_task_execution *
 gzochid_transactional_application_task_timed_execution_new 
 (gzochid_application_task *task, struct timeval timeout)
 {
-  return NULL;
+  return execution_new (task, &timeout);
 }
 
 void gzochid_serialize_application_task 
@@ -79,8 +105,46 @@ void gzochid_transactional_application_task_execution_free
 {
 }
 
+static void transactional_task_worker (gpointer data)
+{
+  void **args = (void **) data;
+  
+  gzochid_application_context *context = 
+    (gzochid_application_context *) args[0];
+  gzochid_auth_identity *identity = (gzochid_auth_identity *) args[1];
+  gzochid_application_task *task = (gzochid_application_task *) args[2];
+  
+  task->worker (context, identity, task->data);
+}
+
+void gzochid_application_transactional_task_worker 
+(gzochid_application_context *context, gzochid_auth_identity *identity, 
+ gpointer data)
+{
+  void *args[3];
+  gzochid_transactional_application_task_execution *execution = 
+    (gzochid_transactional_application_task_execution *) data;
+
+  args[0] = context;
+  args[1] = identity;
+  args[2] = execution->task;
+
+  if (execution->timeout != NULL)
+    execution->result = gzochid_transaction_execute_timed 
+      (transactional_task_worker, args, *execution->timeout);
+  else execution->result = 
+         gzochid_transaction_execute (transactional_task_worker, args);
+
+  execution->attempts++;
+}
+
 void gzochid_application_resubmitting_transactional_task_worker 
 (gzochid_application_context *app_context, gzochid_auth_identity *identity, 
  gpointer data)
 {
+  gzochid_transactional_application_task_execution *execution = 
+    (gzochid_transactional_application_task_execution *) data;
+  
+  gzochid_application_transactional_task_worker 
+    (app_context, identity, execution);
 }
