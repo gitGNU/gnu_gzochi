a9508939257eb315043caaa1deda83d469b4a331
Julian Graham <julian@rake>
2014-08-04 01:38:04 -0400

    Fix long-standing issue with console corruption in AberMUD example game.
    (N)curses is not thread-safe out of the box, and it's not easy to make it
    so. The provided mutex-callback functions (e.g., `use_window') don't seem
    to work with blocking calls that may perturb state (e.g., `wgetch').
    Instead, tighten up the client's use of the API such that it all happens
    from a single thread via a primitive "dispatch" mechanism using locks and
    monitors.
    
    * examples/abermud/client/src/console-curses.c
      (abermud_console_event_type): New enum.
      (console_event_lock, console_event_cond, console_event_type,
      console_event_text): New static globals.
      (abermud_console_init): Set halfdelay = 1.
      (append_output, set_prompt, set_title, clear_console,
      dispatch_console_event): New functions.
      (abermud_console_append_output, abermud_console_set_prompt,
      abermud_console_set_title): Rewrite such that they queue a console update
      event instead of doing the update themselves.
      (abermud_console_getch): Rewrite with a loop that polls for input and
      dispatches any pending console events.

--- examples/abermud/client/src/console-curses.c
+++ examples/abermud/client/src/console-curses.c
@@ -1,5 +1,5 @@
 /* console-curses.c: Curses integration for gzochi abermud example game
- * Copyright (C) 2012 Julian Graham
+ * Copyright (C) 2014 Julian Graham
  *
  * This is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -16,6 +16,8 @@
  */
 
 #include <curses.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -25,11 +27,35 @@
 #define OUTPUT_HEIGHT LINES - 7
 #define INPUT_HEIGHT 6
 
-/* Initialize the console and return a pointer to a new console state object
-   that shares input state with the game context via the abermud_input_buffer 
-   INPUT_BUFFER. */
+/*
+  A simple mechanism for enqueuing requests to update the console. Ncurses is
+  not (easily) thread-safe, so this ensures that all calls to the Ncurses API
+  and which may mutate Ncurses global state originate from the same thread.
+ */
 
-abermud_console_state *abermud_console_init (abermud_input_buffer *input_buffer)
+typedef enum 
+  {
+    ABERMUD_CONSOLE_NONE,
+    ABERMUD_CONSOLE_APPEND_OUTPUT,
+    ABERMUD_CONSOLE_SET_TITLE,
+    ABERMUD_CONSOLE_SET_PROMPT
+  }
+  abermud_console_event_type;
+
+static pthread_mutex_t console_event_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t console_event_cond = PTHREAD_COND_INITIALIZER;
+
+static abermud_console_event_type console_event_type = ABERMUD_CONSOLE_NONE;
+static char *console_event_text = NULL;
+
+/* 
+   Initialize the console and return a pointer to a new console state object
+   that shares input state with the game context via the abermud_input_buffer 
+   INPUT_BUFFER. 
+ */
+
+abermud_console_state *
+abermud_console_init (abermud_input_buffer *input_buffer)
 {
   abermud_console_state *state = malloc (sizeof (abermud_console_state));
   
@@ -49,30 +75,38 @@ abermud_console_state *abermud_console_init (abermud_input_buffer *input_buffer)
   keypad (state->input, TRUE);
   noecho ();
 
+  /* Limit the amount of time Ncurses will wait for input to 1 tenth of a 
+     second. */
+
+  halfdelay (1);
+
   return state;
 }
 
-/* Appends the content of OUTPUT to the console's server output window. The
-   output window will be scrolled up as necessary to accommodate the lines in
-   OUTPUT. */
+/*
+  Appends the content of OUTPUT to the console's server output window. The
+  output window will be scrolled up as necessary to accommodate the lines in
+  OUTPUT. 
+ */
 
-void abermud_console_append_output (abermud_console_state *state, char *output)
+static void 
+append_output (abermud_console_state *state)
 {
-  int i = 0, len = strlen (output);
+  int i = 0, len = strlen (console_event_text);
   
   scroll (state->output);
   wmove (state->output, OUTPUT_HEIGHT - 1, 0);
   
   for (; i < len; i++) 
     {
-      if (output [i] == '\n')
+      if (console_event_text [i] == '\n')
 	{
 	  scroll (state->output);
 	  wmove (state->output, OUTPUT_HEIGHT - 1, 0);
 	  wclrtoeol (state->output);
 	  wmove (state->output, OUTPUT_HEIGHT - 1, 0);
 	}
-      else waddch (state->output, output[i]);
+      else waddch (state->output, console_event_text[i]);
     }
 
   wrefresh (state->output);
@@ -83,21 +117,34 @@ void abermud_console_append_output (abermud_console_state *state, char *output)
   abermud_console_input_cursor_to (state, state->input_buffer->pos);
 }
 
-/* Sets the console's input prompt to the contents of PROMPT and redraws the 
-   input window, including the current contents of the input buffer. The 
-   console stores a copy of PROMPT to allow the input area to be redrawn as 
-   necessary. */
+/*
+  Enqueue a request to add output to the console.
+ */
 
-void abermud_console_set_prompt
-(abermud_console_state *state, char *prompt)
+void 
+abermud_console_append_output (abermud_console_state *state, char *output)
 {
-  /* Free any existiing prompt data. */
+  pthread_mutex_lock (&console_event_lock);
+  
+  while (console_event_type != ABERMUD_CONSOLE_NONE)
+    pthread_cond_wait (&console_event_cond, &console_event_lock);
 
-  if (state->prompt != NULL)
-    free (state->prompt);
+  console_event_type = ABERMUD_CONSOLE_APPEND_OUTPUT;
+  console_event_text = strdup (output);
 
-  state->prompt = strndup (prompt, strlen (prompt));
+  pthread_mutex_unlock (&console_event_lock);
+}
 
+/*
+  Sets the console's input prompt to the contents of PROMPT and redraws the 
+  input window, including the current contents of the input buffer. The 
+  console stores a copy of PROMPT to allow the input area to be redrawn as 
+  necessary. 
+ */
+
+static void 
+set_prompt (abermud_console_state *state)
+{
   werase (state->input);
   wprintw (state->input, "%s", state->prompt);
   wmove (state->input, 0, strlen (state->prompt));
@@ -106,16 +153,42 @@ void abermud_console_set_prompt
   wrefresh (state->input);
 }
 
-/* Set the console's title text (i.e., the content of the title window) to the
-   contents of TITLE. */
+/*
+  Enqueue a request to set the console prompt.
+ */
 
-void abermud_console_set_title (abermud_console_state *state, char *title)
+void 
+abermud_console_set_prompt (abermud_console_state *state, char *prompt)
 {
-  int i = 0, len = strlen (title);
+  pthread_mutex_lock (&console_event_lock);
+  
+  /* Free any existing prompt data. */
+
+  if (state->prompt != NULL)
+    free (state->prompt);
+
+  state->prompt = strndup (prompt, strlen (prompt));
+
+  while (console_event_type != ABERMUD_CONSOLE_NONE)
+    pthread_cond_wait (&console_event_cond, &console_event_lock);
+
+  console_event_type = ABERMUD_CONSOLE_SET_PROMPT;
+  pthread_mutex_unlock (&console_event_lock);
+}
+
+/*
+  Set the console's title text (i.e., the content of the title window) to the
+  contents of TITLE. 
+ */
+
+static void 
+set_title (abermud_console_state *state)
+{
+  int i = 0, len = strlen (console_event_text);
   
   wattron (state->title, A_REVERSE);
   wclrtoeol (state->title);
-  mvwaddnstr (state->title, 0, 0, title, len);
+  mvwaddnstr (state->title, 0, 0, console_event_text, len);
   
   for (; i < COLS - len; i++) 
     waddch (state->title, ' ');
@@ -124,11 +197,35 @@ void abermud_console_set_title (abermud_console_state *state, char *title)
   wrefresh (state->title);
 }
 
-/* Renders the contents of the console's input area (i.e., everything following
-   the prompt) based on the state of the shared input buffer. This function 
-   should be called whenever the state of the input buffer changes. */
+/*
+  Enqueue a request to set the title.
+ */
 
-void abermud_console_draw_input (abermud_console_state *state)
+void 
+abermud_console_set_title (abermud_console_state *state, char *title)
+{
+  pthread_mutex_lock (&console_event_lock);
+  
+  while (console_event_type != ABERMUD_CONSOLE_NONE)
+    pthread_cond_wait (&console_event_cond, &console_event_lock);
+
+  console_event_type = ABERMUD_CONSOLE_SET_TITLE;
+  console_event_text = strdup (title);
+
+  pthread_mutex_unlock (&console_event_lock);  
+}
+
+/*
+  Renders the contents of the console's input area (i.e., everything following
+  the prompt) based on the state of the shared input buffer. This function 
+  should be called whenever the state of the input buffer changes. 
+
+  This function currently does its work directly (instead of enqueueing it)
+  because it is only called from the Ncurses thread.
+*/
+
+void 
+abermud_console_draw_input (abermud_console_state *state)
 {
   wmove (state->input, 0, strlen (state->prompt));
   wclrtoeol (state->input);
@@ -137,11 +234,13 @@ void abermud_console_draw_input (abermud_console_state *state)
   wrefresh (state->input);
 }
 
-/* Send the console's cursor to the specified offset OFFSET within the input 
-   area, relative to the current prompt. */
+/*
+  Send the console's cursor to the specified offset OFFSET within the input 
+  area, relative to the current prompt. 
+*/
 
-void abermud_console_input_cursor_to
-(abermud_console_state *state, int offset)
+void 
+abermud_console_input_cursor_to (abermud_console_state *state, int offset)
 {
   int x, y;
   int pos = offset;
@@ -159,20 +258,75 @@ void abermud_console_input_cursor_to
   wrefresh (state->input);
 }
 
-/* Shuts down the console, freeing all the resources (such as curses WINDOWs) 
-   that have been allocated to it. `abermud_console_init' must be called before
-   further console operations can be performed. */
+/*
+  Shuts down the console, freeing all the resources (such as curses WINDOWs) 
+  that have been allocated to it. `abermud_console_init' must be called before
+  further console operations can be performed. 
+ */
 
-void abermud_console_shutdown (abermud_console_state *state)
+void 
+abermud_console_shutdown (abermud_console_state *state)
 {
   endwin ();
   free (state);
 }
 
-/* Reads and returns a character from the input area (i.e., the input window)
-   of the console. */
+/*
+  Empty the console event state to indicate that a new event may be enqueued.
+ */
 
-int abermud_console_getch (abermud_console_state *state)
+static void 
+clear_console_event (void)
 {
-  return wgetch (state->input);
+  console_event_type = ABERMUD_CONSOLE_NONE;
+
+  if (console_event_text != NULL)
+    {
+      free (console_event_text);
+      console_event_text = NULL;
+    }
+}
+
+/*
+  Dispatch the enqueued event, if any.
+ */
+
+static void 
+dispatch_console_event (abermud_console_state *state)
+{
+  pthread_mutex_lock (&console_event_lock);
+
+  switch (console_event_type)
+    {
+    case ABERMUD_CONSOLE_APPEND_OUTPUT: append_output (state); break;
+    case ABERMUD_CONSOLE_SET_TITLE: set_title (state); break;
+    case ABERMUD_CONSOLE_SET_PROMPT: set_prompt (state); break;
+    default: break;
+    }
+
+  clear_console_event ();
+
+  pthread_cond_signal (&console_event_cond);
+  pthread_mutex_unlock (&console_event_lock);
+}
+
+/*
+  Reads and returns a character from the input area (i.e., the input window)
+  of the console. Since the Ncurses input system is set to wait for input for a
+  maximum of 100ms (see above), dispatch any waiting console events in between
+  waiting for input.
+ */
+
+int 
+abermud_console_getch (abermud_console_state *state)
+{
+  while (TRUE)
+    {
+      int ret = wgetch (state->input);
+
+      if (ret != ERR)
+	return ret;
+
+      dispatch_console_event (state);
+    }
 }
