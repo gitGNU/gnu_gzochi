0357e12f421eaa073ee6e1c5f649db2d555ea232
Julian Graham <julian@rake>
2015-03-25 10:40:42 -0400

    Define the interface and associated types for dynamic storage engines.
    * gzochi-server/src/storage.h (gzochid_storage_engine_interface,
      gzochid_storage_engine): New struct typedefs.
      (GZOCHID_STORAGE_INIT_ENGINE): New pre-processor macro.

--- gzochi-server/src/storage.h
+++ gzochi-server/src/storage.h
@@ -1,5 +1,5 @@
 /* storage.h: Prototypes and declarations for storage.c
- * Copyright (C) 2014 Julian Graham
+ * Copyright (C) 2015 Julian Graham
  *
  * gzochi is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 #define GZOCHID_STORAGE_H
 
 #include <glib.h>
+#include <gmodule.h>
 #include <gmp.h>
 #include <sys/time.h>
 
@@ -29,26 +30,31 @@
 #define GZOCHID_STORAGE_CREATE 0x01
 #define GZOCHID_STORAGE_EXCL 0x02
 
-typedef struct _gzochid_storage_context
+struct _gzochid_storage_context
 {
   gpointer environment;
-} gzochid_storage_context;
+};
 
-typedef struct _gzochid_storage_store
+typedef struct _gzochid_storage_context  gzochid_storage_context;
+
+struct _gzochid_storage_store
 {
   GMutex mutex;
   gzochid_storage_context *context;
   gpointer database;
-} gzochid_storage_store;
+};
 
-typedef struct _gzochid_storage_transaction
+typedef struct _gzochid_storage_store gzochid_storage_store;
+
+struct _gzochid_storage_transaction
 {
   gzochid_storage_context *context;
   gpointer txn;
   gboolean rollback;
   gboolean should_retry;
-} gzochid_storage_transaction;
+};
 
+typedef struct _gzochid_storage_transaction gzochid_storage_transaction;
 gzochid_storage_context *gzochid_storage_initialize (char *);
 
 /**
@@ -109,5 +115,88 @@ char *gzochid_storage_transaction_first_key
 char *gzochid_storage_transaction_next_key 
 (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t, 
  size_t *);
+
+/* The interface provided by storage engine modules. */
+
+struct _gzochid_storage_engine_interface
+{
+  char *name;
+
+  gzochid_storage_context *(*initialize) (char *);
+
+  /* Close the specified storage context. */
+
+  void (*close_context) (gzochid_storage_context *);
+
+  /* Destroy the storage context rooted at the specified path by removing all
+     associated files, including the path itself. */
+
+  void (*destroy_context) (char *);
+  gzochid_storage_store *(*open) 
+    (gzochid_storage_context *, char *, unsigned int);
+  void (*close_store) (gzochid_storage_store *);
+
+  /* Destroy the store rooted at the specified storage context and path by 
+     removing all associated files. */
+
+  void (*destroy_store) (gzochid_storage_context *, char *);
+  void (*lock) (gzochid_storage_store *);
+  void (*unlock) (gzochid_storage_store *);
+
+  char *(*get) (gzochid_storage_store *, char *, size_t, size_t *);
+  void (*put) (gzochid_storage_store *, char *, size_t, char *, size_t);
+  int (*delete) (gzochid_storage_store *, char *, size_t);
+  char *(*first_key) (gzochid_storage_store *, size_t *);
+  char *(*next_key) (gzochid_storage_store *, char *, size_t, size_t *);
+
+  gzochid_storage_transaction *(*transaction_begin) (gzochid_storage_context *);
+  gzochid_storage_transaction *(*transaction_begin_timed)
+    (gzochid_storage_context *, struct timeval);
+  void (*transaction_commit) (gzochid_storage_transaction *);
+  void (*transaction_rollback) (gzochid_storage_transaction *);
+  void (*transaction_prepare) (gzochid_storage_transaction *);
+  char *(*transaction_get) 
+    (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t, 
+     size_t *);
+  char *(*transaction_get_for_update)
+    (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t, 
+     size_t *);
+  void (*transaction_put)
+    (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t, 
+     char *, size_t);
+  int (*transaction_delete)
+    (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t);
+  char *(*transaction_first_key)
+    (gzochid_storage_transaction *, gzochid_storage_store *, size_t *);
+  char *(*transaction_next_key)
+    (gzochid_storage_transaction *, gzochid_storage_store *, char *, size_t, 
+     size_t *);
+};
+
+typedef struct _gzochid_storage_engine_interface 
+gzochid_storage_engine_interface;
+
+/* An instance of a storage engine that has been loaded as a dynamic module. */
+
+struct _gzochid_storage_engine
+{
+  GModule *handle; /* The dynamic module handle. */
+  gzochid_storage_engine_interface *interface; /* The storage interface. */
+};
+
+typedef struct _gzochid_storage_engine gzochid_storage_engine;
+
+/* Define and export the storage engine bootstrap function. This macro is 
+   intended for use by the author of a storage engine module. */
+
+#define GZOCHID_STORAGE_INIT_ENGINE(interface)	   \
+  G_MODULE_EXPORT gint gzochid_storage_init_engine \
+  (gzochid_storage_engine *engine);		   \
+  G_MODULE_EXPORT gint gzochid_storage_init_engine \
+  (gzochid_storage_engine *engine)		   \
+  {						   \
+    engine->interface = &(interface);		   \
+    return 0;					   \
+  }
 
 #endif /* GZOCHID_STORAGE_H */
