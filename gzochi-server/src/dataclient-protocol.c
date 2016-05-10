987d2d278afd4411b7049f886a52c4b97279cc0b
Julian Graham <julian@rake>
2016-05-09 23:33:58 -0400

    Complete implementation of dataclient client protocol.
    * gzochi-server/src/dataclient-protocol.c (can_dispatch, client_dispatch):
      Add real implementations similar to other client protocols.
      (dispatch_oids_response, dispatch_value_response,
      dispatch_next_key_response, dispatch_message): New functions.
    * gzochi-server/tests/Makefile.am (test_programs): Add
      `test-dataclient-protocol'.
      (test_dataclient_protocol_CFLAGS, test_dataclient_protocol_SOURCES,
      test_dataclient_protocol_LDADD): New declarations.
    * gzochi-server/tests/test-dataclient-protocol.c: New file.

--- gzochi-server/src/dataclient-protocol.c
+++ gzochi-server/src/dataclient-protocol.c
@@ -17,27 +17,193 @@
 
 #include <glib.h>
 #include <gzochi-common.h>
+#include <stddef.h>
+#include <stdlib.h>
 
+#include "data-protocol.h"
 #include "dataclient.h"
 #include "dataclient-protocol.h"
+#include "protocol.h"
+
+/*
+  Returns `TRUE' if the specified buffer has a complete message that it is 
+  ready (with respect to its length) to be dispatched to a handler. The length
+  encoding is similar to the one used for the game protocol: A two-byte, 
+  big-endian prefix giving the length of the message payload minus the opcode,
+  which is the byte directly following the length prefix. 
+  
+  (So the smallest possible message would be three bytes, in which the first 
+  two bytes were `NULL'.) 
+*/
 
 static gboolean
 client_can_dispatch (const GByteArray *buffer, gpointer user_data)
 {
-  return FALSE;
+  return buffer->len >= 3
+    && buffer->len >= gzochi_common_io_read_short (buffer->data, 0) + 3;
 }
+
+/* Processes the message payload following the 
+   `GZOZCHID_DATA_PROTOCOL_OIDS_RESPONSE' opcode. Returns `TRUE' if the 
+   message was successfully decoded, `FALSE' otherwise. */
+
+static gboolean
+dispatch_oids_response (GzochidDataClient *client, const unsigned char *data,
+			unsigned short len)
+{
+  GBytes *response_bytes = g_bytes_new_with_free_func (data, len, NULL, NULL);
+  gzochid_data_reserve_oids_response *response =
+    gzochid_data_protocol_reserve_oids_response_read (response_bytes);
+  gboolean ret = TRUE;
+
+  if (response == NULL)
+    ret = FALSE;
+  else
+    {
+      /* Invoke the waiting callback. */
+
+      gzochid_dataclient_received_oids (client, response);
+      gzochid_data_reserve_oids_response_free (response);
+    }
+  
+  g_bytes_unref (response_bytes);
+
+  return ret;
+}
+
+/* Processes the message payload following the 
+   `GZOZCHID_DATA_PROTOCOL_VALUE_RESPONSE' opcode. Returns `TRUE' if the 
+   message was successfully decoded, `FALSE' otherwise. */
+
+static gboolean
+dispatch_value_response (GzochidDataClient *client, const unsigned char *data,
+			 unsigned short len)
+{
+  GBytes *response_bytes = g_bytes_new_with_free_func (data, len, NULL, NULL);
+  gzochid_data_response *response =
+    gzochid_data_protocol_response_read (response_bytes);
+  gboolean ret = TRUE;
+
+  if (response == NULL)
+    ret = FALSE;
+  else
+    {
+      /* Invoke any waiting callbacks. */
+
+      gzochid_dataclient_received_value (client, response);
+      gzochid_data_response_free (response);
+    }
+  
+  g_bytes_unref (response_bytes);
+
+  return ret;
+}
+
+/* Processes the message payload following the 
+   `GZOZCHID_DATA_PROTOCOL_NEXT_KEY_RESPONSE' opcode. Returns `TRUE' if the 
+   message was successfully decoded, `FALSE' otherwise. */
+
+static gboolean
+dispatch_next_key_response (GzochidDataClient *client,
+			    const unsigned char *data, unsigned short len)
+{
+  GBytes *response_bytes = g_bytes_new_with_free_func (data, len, NULL, NULL);
+  gzochid_data_response *response =
+    gzochid_data_protocol_response_read (response_bytes);
+  gboolean ret = TRUE;
+
+  if (response == NULL)
+    ret = FALSE;
+  else
+    {
+      /* Invoke any waiting callbacks. */
+      
+      gzochid_dataclient_received_next_key (client, response);
+      gzochid_data_response_free (response);
+    }
+
+  g_bytes_unref (response_bytes);
+
+  return ret;
+}
+
+/* Attempt to dispatch a fully-buffered message from the server based on the 
+   message opcode. */
+
+static void 
+dispatch_message (GzochidDataClient *client, unsigned char *message,
+		  unsigned short len)
+{
+  int opcode = message[0];
+  unsigned char *payload = message + 1;
+  
+  len--;
+  
+  switch (opcode)
+    {
+    case GZOCHID_DATA_PROTOCOL_OIDS_RESPONSE:
+      dispatch_oids_response (client, payload, len);
+      break;      
+    case GZOCHID_DATA_PROTOCOL_VALUE_RESPONSE:
+      dispatch_value_response (client, payload, len);
+      break;
+    case GZOCHID_DATA_PROTOCOL_NEXT_KEY_RESPONSE:
+      dispatch_next_key_response (client, payload, len);
+      break;
+      
+    default:
+      g_warning ("Unexpected opcode %d received from server", opcode);
+    }
+  
+  return;
+}
+
+/* Attempts to dispatch all messages in the specified buffer. Returns the 
+   number of bytes consumed from the buffer. */
 
 static unsigned int
 client_dispatch (const GByteArray *buffer, gpointer user_data)
 {
-  return 0;
+  GzochidDataClient *client = user_data;
+
+  int offset = 0, total = 0;
+  int remaining = buffer->len;
+
+  while (remaining >= 3)
+    {
+      unsigned short len = gzochi_common_io_read_short
+        ((unsigned char *) buffer->data, offset);
+      
+      if (++len > remaining - 2)
+        break;
+      
+      offset += 2;
+
+      dispatch_message (client, (unsigned char *) buffer->data + offset, len);
+      
+      offset += len;
+      remaining -= len + 2;
+      total += len + 2;
+    }
+
+  return total;
 }
+
+/* The client error handler. Notifies the data client that its connection to the
+   meta server is no longer valid. */
 
 static void
 client_error (gpointer user_data)
 {
   gzochid_dataclient_nullify_connection (user_data);
 }
+
+/*
+  Client finalization callback. 
+
+  This is currently a no-op, as the data client protocol has no state other than
+  the data client, to which it does not even hold a reference.
+*/
 
 static void
 client_free (gpointer user_data)
---
+++ gzochi-server/tests/test-dataclient-protocol.c
@@ -0,0 +1,240 @@
+/* test-dataclient-protocol.c: Tests for dataclient-protocol.c in gzochid.
+ * Copyright (C) 2016 Julian Graham
+ *
+ * gzochi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include <glib-object.h>
+#include <stddef.h>
+
+#include "dataclient.h"
+#include "dataclient-protocol.h"
+
+struct _GzochidDataClient
+{
+  GObject parent_instance;
+
+  GList *activity_log;
+  gboolean connected;
+};
+
+G_DEFINE_TYPE (GzochidDataClient, gzochid_data_client, G_TYPE_OBJECT);
+
+static void
+gzochid_data_client_finalize (GObject *gobject)
+{
+  GzochidDataClient *client = GZOCHID_DATA_CLIENT (gobject);
+
+  g_list_free_full (client->activity_log, g_free);
+}
+
+static void
+gzochid_data_client_class_init (GzochidDataClientClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = gzochid_data_client_finalize;
+}
+
+static void
+gzochid_data_client_init (GzochidDataClient *self)
+{
+  self->connected = TRUE;
+}
+
+void
+gzochid_dataclient_nullify_connection (GzochidDataClient *client)
+{
+  client->connected = FALSE;
+}
+
+void
+gzochid_dataclient_received_oids (GzochidDataClient *client,
+				  gzochid_data_reserve_oids_response *response)
+{
+  client->activity_log = g_list_append
+    (client->activity_log,
+     g_strdup_printf ("RECEIVED OID BLOCK %s/%lu:%lu", response->app,
+		      mpz_get_ui (response->block.block_start),
+		      response->block.block_size));
+}
+
+void
+gzochid_dataclient_received_value (GzochidDataClient *client,
+				   gzochid_data_response *response)
+{
+  client->activity_log = g_list_append
+    (client->activity_log,
+     g_strdup_printf ("RECEIVED VALUE %s/%s:%s", response->app, response->store,
+		      (char *) g_bytes_get_data (response->data, NULL)));
+}
+
+void
+gzochid_dataclient_received_next_key (GzochidDataClient *client,
+				      gzochid_data_response *response)
+{
+  client->activity_log = g_list_append
+    (client->activity_log,
+     g_strdup_printf ("RECEIVED KEY %s/%s:%s", response->app, response->store,
+		      (char *) g_bytes_get_data (response->data, NULL)));
+}
+
+static void
+test_client_can_dispatch_true ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append
+    (bytes, "\x00\x11\x51test\x00oids\x00\x01\x00\x04""foo\x00", 20);
+  
+  g_assert_true
+    (gzochid_dataclient_client_protocol.can_dispatch (bytes, client));
+
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_can_dispatch_false ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append
+    (bytes, "\x00\x11\x51test\x00oids\x00\x01\x00\x04", 16);
+
+  g_assert_false
+    (gzochid_dataclient_client_protocol.can_dispatch (bytes, client));
+
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_dispatch_one_oids_response ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append (bytes, "\x00\x09\x50test\x00""1\x00\x00\x64", 12);
+  
+  g_assert_cmpint
+    (gzochid_dataclient_client_protocol.dispatch (bytes, client), ==, 12);
+  
+  g_assert_cmpint (g_list_length (client->activity_log), ==, 1);
+  g_assert_cmpstr
+    (client->activity_log->data, ==, "RECEIVED OID BLOCK test/1:100");
+  
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_dispatch_one_value_response ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append
+    (bytes, "\x00\x12\x51test\x00names\x00\x01\x00\x04""foo\x00", 21);
+
+  g_assert_cmpint
+    (gzochid_dataclient_client_protocol.dispatch (bytes, client), ==, 21);
+  g_assert_cmpint (g_list_length (client->activity_log), ==, 1);
+  g_assert_cmpstr
+    (client->activity_log->data, ==, "RECEIVED VALUE test/names:foo");
+
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_dispatch_one_next_key_response ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append
+    (bytes, "\x00\x12\x52test\x00names\x00\x01\x00\x04""foo\x00", 21);
+
+  g_assert_cmpint
+    (gzochid_dataclient_client_protocol.dispatch (bytes, client), ==, 21);
+  g_assert_cmpint (g_list_length (client->activity_log), ==, 1);
+  g_assert_cmpstr
+    (client->activity_log->data, ==, "RECEIVED KEY test/names:foo");
+
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_dispatch_multiple ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  GByteArray *bytes = g_byte_array_new ();
+
+  g_byte_array_append
+    (bytes, "\x00\x12\x51test\x00names\x00\x01\x00\x04""foo\x00", 21);
+  g_byte_array_append
+    (bytes, "\x00\x12\x52test\x00names\x00\x01\x00\x04""bar\x00", 21);
+
+  g_assert_cmpint
+    (gzochid_dataclient_client_protocol.dispatch (bytes, client), ==, 42);
+  g_assert_cmpint (g_list_length (client->activity_log), ==, 2);
+  g_assert_cmpstr
+    (client->activity_log->data, ==, "RECEIVED VALUE test/names:foo");
+  g_assert_cmpstr
+    (client->activity_log->next->data, ==, "RECEIVED KEY test/names:bar");
+  
+  g_byte_array_unref (bytes);
+  g_object_unref (client);
+}
+
+static void
+test_client_error ()
+{
+  GzochidDataClient *client = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  
+  gzochid_dataclient_client_protocol.error (client);
+  g_assert_false (client->connected);
+  
+  g_object_unref (client);
+}
+
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/client/can_dispatch/true", test_client_can_dispatch_true);
+  g_test_add_func
+    ("/client/can_dispatch/false", test_client_can_dispatch_false);
+
+  g_test_add_func
+    ("/client/dispatch/one/oids-response",
+     test_client_dispatch_one_oids_response);
+  g_test_add_func
+    ("/client/dispatch/one/value-response",
+     test_client_dispatch_one_value_response);
+  g_test_add_func
+    ("/client/dispatch/one/next-key-response",
+     test_client_dispatch_one_next_key_response);
+
+  g_test_add_func ("/client/dispatch/multiple", test_client_dispatch_multiple);
+  g_test_add_func ("/client/error", test_client_error);
+  
+  return g_test_run ();
+}
