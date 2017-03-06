bb8dec6cb23feb90ab8bfc23ab61809da04acd83
Julian Graham <julian@rake>
2017-03-06 17:13:43 -0500

    Avoid leaking memory when transaction times out while allocating oids.
    Note that there's still a problem where an oid block may be orphaned when
    this happens. Can take steps to avoid this if it becomes a problem.
    
    * gzochi-server/src/oids-dataclient.c (oids_pending_response): Remove
      struct field `dest'; new struct fields `block', `ref_count'.
      (create_oids_pending_response, oids_pending_response_ref,
      oids_pending_response_unref): New function.
      (oids_callback): Unref the response.
      (allocate): Allocate a response object to the heap and unref it on the
      way out.
    * gzochi-server/tests/test-oids-dataclient.c (ignore_warnings,
      reserve_oids_inner, gzochid_data_client_finalize, reserve_oids_timeout,
      noop_prepare, noop_commit_rollback, oids_dataclient_timeout_inner,
      test_oids_dataclient_timeout): New functions.
      (GzochidDataClient): New struct fields `reserve_oids', `thread', `mutex',
      `cond'.
      (gzochid_data_client_class_init): Register finalizer.
      (gzochid_data_client_init): Initialize the mutex and condition variable.
      (gzochid_dataclient_reserve_oids): Invoke the custom reservation
      function.
      (main): Register `/oids-dataclient/reserve/timeout'.

--- gzochi-server/src/oids-dataclient.c
+++ gzochi-server/src/oids-dataclient.c
@@ -25,6 +25,11 @@
 #include "oids-dataclient.h"
 #include "tx.h"
 
+#ifdef G_LOG_DOMAIN
+#undef G_LOG_DOMAIN
+#endif /* G_LOG_DOMAIN */
+#define G_LOG_DOMAIN "gzochid.oids-dataclient"
+
 /* Contextual data needed by the allocation strategy. */
 
 struct _gzochid_oid_dataclient_context
@@ -40,14 +45,19 @@ typedef struct _gzochid_oid_dataclient_context gzochid_oid_dataclient_context;
 
 struct _oids_pending_response
 {
-  /* A target block to fill out with oid block reservation data. */
+  /* A block to fill out with oid block reservation data. */
 
-  gzochid_data_oids_block *dest; 
+  gzochid_data_oids_block block; 
 
   gboolean complete; /* To distinguish spurious wakeups. */
   
   GMutex mutex; /* Mutex for synchronization access to response condition. */
   GCond cond; /* Condition variable for coordinating access to the response. */
+
+  /* Reference count to allow callbacks and cleanup to occur asynchronously 
+     with allocation request. */
+
+  int ref_count; 
 };
 
 typedef struct _oids_pending_response oids_pending_response;
@@ -62,6 +72,50 @@ cleanup_context (gpointer user_data)
   g_object_unref (context->dataclient);
   free (context->app);
   free (context);
+}
+
+/* Create and return a new pending response object. The reference count on the
+   object is initialized to 1. */
+
+static oids_pending_response *
+create_oids_pending_response ()
+{
+  oids_pending_response *pending_response =
+    malloc (sizeof (oids_pending_response));
+  
+  pending_response->block = (gzochid_data_oids_block) { 0 };
+  pending_response->complete = FALSE;
+  pending_response->ref_count = 1;
+  
+  g_mutex_init (&pending_response->mutex);
+  g_cond_init (&pending_response->cond);
+
+  return pending_response;
+}
+
+/* Increase the reference count of the specified `oids_pending_response' 
+   object and return the object. */
+
+static oids_pending_response *
+oids_pending_response_ref (oids_pending_response *pending_response)
+{
+  pending_response->ref_count++;
+  return pending_response;
+}
+
+/* Decrease the referennce count of the specified `oids_pending_response' 
+   object. If the reference count reaches zero, the memory used by the object 
+   is freed. */
+
+static void
+oids_pending_response_unref (oids_pending_response *pending_response)
+{
+  if (--pending_response->ref_count == 0)
+    {
+      g_mutex_clear (&pending_response->mutex);
+      g_cond_clear (&pending_response->cond);
+      free (pending_response);
+    }
 }
 
 /* Callback function for `gzochid_dataclient_reserve_oids. */
@@ -79,13 +133,16 @@ oids_callback (gzochid_data_oids_block block, gpointer user_data)
   
   /* Copy the block metadata to the callback object. */
 
-  response->dest->block_start = block.block_start;
-  response->dest->block_size = block.block_size;
+  response->block = block;
 
   /* Let the caller know the response is complete. */
   
   g_cond_signal (&response->cond);
   g_mutex_unlock (&response->mutex);
+
+  /* Unref the response object. */
+  
+  oids_pending_response_unref (response);
 }
 
 /* The allocation function implementation. */
@@ -93,8 +150,9 @@ oids_callback (gzochid_data_oids_block block, gpointer user_data)
 static gboolean
 allocate (gpointer user_data, gzochid_data_oids_block *block, GError **err)
 {
+  gboolean complete = FALSE;
   gzochid_oid_dataclient_context *context = user_data;
-  oids_pending_response pending_response;
+  oids_pending_response *pending_response = NULL;
 
   if (gzochid_transaction_active () && gzochid_transaction_rollback_only ())
     {
@@ -105,45 +163,55 @@ allocate (gpointer user_data, gzochid_data_oids_block *block, GError **err)
     }
   
   /* Create and initialize the pending response structure. */
-  
-  pending_response.dest = block;
-  pending_response.complete = FALSE;
-  g_mutex_init (&pending_response.mutex);
-  g_cond_init (&pending_response.cond);
+
+  pending_response = create_oids_pending_response ();
 
   /* Take the lock and submit the request for oids. */
   
-  g_mutex_lock (&pending_response.mutex);
-
+  g_mutex_lock (&pending_response->mutex);
+  
   gzochid_dataclient_reserve_oids
-    (context->dataclient, context->app, oids_callback, &pending_response);
+    (context->dataclient, context->app, oids_callback,
+     oids_pending_response_ref (pending_response));
 
   if (gzochid_transaction_active () && gzochid_transaction_timed ())
 
     /* Wait on the pending response condition in a loop to handle spurious 
        wakeups. */
 
-    while (!pending_response.complete && !gzochid_transaction_timed_out ())
+    while (!pending_response->complete && !gzochid_transaction_timed_out ())
       {
 	struct timeval remaining = gzochid_transaction_time_remaining ();
 	gint64 end_timestamp = g_get_monotonic_time () + remaining.tv_sec * 1000
 	  + remaining.tv_usec;
 	
 	if (!g_cond_wait_until
-	    (&pending_response.cond, &pending_response.mutex, end_timestamp))
-	  g_debug ("Transaction timed out while waiting for oid block.");
+	    (&pending_response->cond, &pending_response->mutex, end_timestamp))
+	  {
+	    g_debug ("Transaction timed out while waiting for oid block.");
+	    g_set_error
+	      (err, GZOCHID_OIDS_ERROR, GZOCHID_OIDS_ERROR_TRANSACTION,
+	       "Transaction timed out while waiting for oid block.");
+	  }
       }
-  else while (!pending_response.complete)
-	 g_cond_wait (&pending_response.cond, &pending_response.mutex);
+  else while (!pending_response->complete)
+	 g_cond_wait (&pending_response->cond, &pending_response->mutex);
 
-  g_mutex_unlock (&pending_response.mutex);
-
-  /* Tear down the response object. */
+  if (pending_response->complete)
+    {
+      block->block_start = pending_response->block.block_start;
+      block->block_size = pending_response->block.block_size;
+      
+      complete = TRUE;
+    }
   
-  g_mutex_clear (&pending_response.mutex);
-  g_cond_clear (&pending_response.cond);
+  g_mutex_unlock (&pending_response->mutex);
 
-  return pending_response.complete;
+  /* Unref the response object. */
+
+  oids_pending_response_unref (pending_response);
+  
+  return complete;
 }
 
 gzochid_oid_allocation_strategy *
--- gzochi-server/tests/test-oids-dataclient.c
+++ gzochi-server/tests/test-oids-dataclient.c
@@ -1,5 +1,5 @@
 /* test-oids-dataclient.c: Tests for oids-dataclient.c in gzochid.
- * Copyright (C) 2016 Julian Graham
+ * Copyright (C) 2017 Julian Graham
  *
  * gzochi is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -19,25 +19,20 @@
 #include <glib-object.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "dataclient.h"
 #include "oids-dataclient.h"
+#include "tx.h"
 
-struct _GzochidDataClient
+static gboolean
+ignore_warnings (const gchar *log_domain, GLogLevelFlags log_level,
+		 const gchar *message, gpointer user_data)
 {
-  GObject parent_instance;
-};
-
-G_DEFINE_TYPE (GzochidDataClient, gzochid_data_client, G_TYPE_OBJECT);
-
-static void
-gzochid_data_client_class_init (GzochidDataClientClass *klass)
-{
-}
-
-static void
-gzochid_data_client_init (GzochidDataClient *self)
-{
+  if (log_level & G_LOG_LEVEL_CRITICAL
+      || log_level & G_LOG_LEVEL_WARNING)
+    return FALSE;
+  else return log_level & G_LOG_FLAG_FATAL;
 }
 
 static gpointer
@@ -45,8 +40,8 @@ reserve_oids_inner (gpointer data)
 {
   gpointer *args = data;
   gzochid_data_oids_block block;
-  gzochid_dataclient_oids_callback callback = args[0];
-  gpointer callback_data = args[1];
+  gzochid_dataclient_oids_callback callback = args[1];
+  gpointer callback_data = args[2];
   
   block.block_start = 1;
   block.block_size = 100;
@@ -54,6 +49,50 @@ reserve_oids_inner (gpointer data)
   callback (block, callback_data);
 
   free (args);
+  return NULL;
+}
+
+struct _GzochidDataClient
+{
+  GObject parent_instance;
+  
+  GThreadFunc reserve_oids;
+  GThread *thread;
+
+  GMutex mutex;
+  GCond cond;
+};
+
+G_DEFINE_TYPE (GzochidDataClient, gzochid_data_client, G_TYPE_OBJECT);
+
+static void
+gzochid_data_client_finalize (GObject *obj)
+{
+  GzochidDataClient *client = GZOCHID_DATA_CLIENT (obj);
+  
+  g_thread_join (client->thread);
+  g_mutex_clear (&client->mutex);
+  g_cond_clear (&client->cond);
+  
+  G_OBJECT_CLASS (gzochid_data_client_parent_class)->finalize (obj);
+}
+
+static void
+gzochid_data_client_class_init (GzochidDataClientClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = gzochid_data_client_finalize;
+}
+
+static void
+gzochid_data_client_init (GzochidDataClient *self)
+{
+  self->reserve_oids = reserve_oids_inner;
+  self->thread = NULL;
+
+  g_mutex_init (&self->mutex);
+  g_cond_init (&self->cond);
 }
 
 void
@@ -61,12 +100,13 @@ gzochid_dataclient_reserve_oids (GzochidDataClient *client, char *app,
 				 gzochid_dataclient_oids_callback callback,
 				 gpointer data)
 {
-  gpointer *args = malloc (sizeof (gpointer) * 2);
+  gpointer *args = malloc (sizeof (gpointer) * 3);
 
-  args[0] = callback;
-  args[1] = data;
+  args[0] = client;
+  args[1] = callback;
+  args[2] = data;
   
-  g_thread_new ("fake-main-loop", reserve_oids_inner, args);
+  client->thread = g_thread_new ("fake-main-loop", client->reserve_oids, args);
 }
 
 static void
@@ -87,6 +127,77 @@ test_oids_dataclient_reserve ()
   g_object_unref (dataclient);
 }
 
+static gpointer
+reserve_oids_timeout (gpointer data)
+{
+  gpointer *args = data;
+  GzochidDataClient *client = GZOCHID_DATA_CLIENT (args[0]);
+
+  g_mutex_lock (&client->mutex);
+  g_cond_signal (&client->cond);
+
+  if (!g_cond_wait_until
+      (&client->cond, &client->mutex, g_get_monotonic_time () + 5000000))
+    g_assert (FALSE);
+  
+  g_mutex_unlock (&client->mutex);
+
+  reserve_oids_inner (data);
+  
+  return NULL;
+}
+
+static int
+noop_prepare (gpointer data)
+{
+  return TRUE;
+}
+
+static void
+noop_commit_rollback (gpointer data)
+{
+}
+
+gzochid_transaction_participant test_participant =
+  { "test", noop_prepare, noop_commit_rollback, noop_commit_rollback };
+
+static void
+oids_dataclient_timeout_inner (gpointer data)
+{
+  GzochidDataClient *dataclient = g_object_new (GZOCHID_TYPE_DATA_CLIENT, NULL);
+  gzochid_oid_allocation_strategy *strategy =
+    gzochid_dataclient_oid_strategy_new (dataclient, "test");
+  gzochid_data_oids_block block;
+  GError *err = NULL;
+
+  dataclient->reserve_oids = reserve_oids_timeout;
+
+  gzochid_transaction_join (&test_participant, NULL);
+
+  g_mutex_lock (&dataclient->mutex);
+  g_assert_false (gzochid_oids_reserve_block (strategy, &block, &err));
+
+  if (!g_cond_wait_until
+      (&dataclient->cond, &dataclient->mutex,
+       g_get_monotonic_time () + 5000000))
+    g_test_fail ();
+
+  g_cond_signal (&dataclient->cond);
+  g_mutex_unlock (&dataclient->mutex);
+  
+  gzochid_oid_allocation_strategy_free (strategy);
+  g_object_unref (dataclient);
+}
+
+static void
+test_oids_dataclient_timeout ()
+{
+  g_test_log_set_fatal_handler (ignore_warnings, NULL);
+
+  gzochid_transaction_execute_timed
+    (oids_dataclient_timeout_inner, NULL, (struct timeval) { 0, 0 });
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -100,6 +211,8 @@ main (int argc, char *argv[])
 
   g_test_add_func
     ("/oids-dataclient/reserve/simple", test_oids_dataclient_reserve);
-
+  g_test_add_func
+    ("/oids-dataclient/reserve/timeout", test_oids_dataclient_timeout);
+  
   return g_test_run ();
 }
